aa6e80fa575b0c1b9548f28d609508f4b69e8d0d
Di Wang <wangdicoder@gmail.com>
2016-12-01 17:56:29 +1100

    add comment for fetch home data

--- js/dao/HomeDataDAO.js
+++ js/dao/HomeDataDAO.js
@@ -23,11 +23,16 @@ export default class HomeDataDAO{
         }
     }
 
-    /**
-     * get local data firstly, then judge the time of local data is today's date.
-     * if the result is true, it will return the local data, 'requestHomeData.js' will not fetch server data.
-     * if false, it also returns the local data, 'requestHomeData.js' will fetch server data, but it will judge
-     * whether the server data is same as local data, which determines whether to save server data.
+    /** Get local data.
+     *
+     * 1. firstly, to judge the time of the local data is today's date.
+     * 2. if the result is true, it will return the local data, 'requestHomeData.js' will not fetch server data.
+     *    if false, it also returns the local data, 'requestHomeData.js' will fetch the server data.
+     * 3. if it returns null(reject null), which means there are no records or something wrong. This
+     *    will also trigger 'requestHomeData.js' to fetch the server data.
+     * 4. when the function rejects the local data(reject data.content), 'requestHomeData.js' will
+     *    fetch the server data and judge the validation of data. Then to determine whether to save
+     *    latest data or still show yesterday's data, since maybe today's data hasn't updated yet.
      */
     fetchLocalData(time){
         return new Promise((resolve, reject) => {
@@ -37,7 +42,6 @@ export default class HomeDataDAO{
                     //judge whether the data is updated
                     if(data) {
                         if (data.time.toString() === time) {
-                            //ToastAndroid.show('fetchLocalSuccess', ToastAndroid.SHORT);
                             resolve(data.content);
                         } else {
                             reject(data.content);
@@ -46,7 +50,6 @@ export default class HomeDataDAO{
                         reject(null);
                     }
                 }else{ // must fetch server data
-                    //ToastAndroid.show('fetchLocalFailed', ToastAndroid.SHORT);
                     reject(null);
                 }
             });
