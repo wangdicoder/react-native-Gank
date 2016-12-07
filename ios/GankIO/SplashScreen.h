3a54a45835e00b9434997019a00bfcb9fa832b69
Di Wang <wangdicoder@gmail.com>
2016-12-07 23:26:29 +1100

    iOS splash screen

---
+++ ios/GankIO/SplashScreen.h
@@ -0,0 +1,15 @@
+//
+//  SplashScreen.h
+//  GankIO
+//
+//  Created by wangdi on 7/12/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+
+@interface SplashScreen : NSObject<RCTBridgeModule>
++ (void)show;
+
+@end
